01b5ba2a6e2a600bad14dcef09a6527de54ea1b3
Nisha2608 <ashoknisha77@gmail.com>
2026-02-16 21:08:53 +0530

    newly added

--- routes/subject.router.js
+++ routes/subject.router.js
@@ -4,15 +4,8 @@ const subjectController = require("../controllers/subject.controller");
 
 // POST - Create Subject
 router.post("/createSubject", subjectController.createSubject);
-
-// GET - Get All Subjects
 router.get("/getAllSubjects", subjectController.getAllSubjects);
-
-// ✅ GET - Get Subjects by School, Grade, and Section
-// routes/subject.router.js
-router.get(
-    "/getSubjectsBySchoolGradeSection/:schoolId/:gradeId/:sectionId",
-    subjectController.getSubjectsBySchoolGradeSection
-  );
+router.get("/getSubjectsBySchoolGradeSection/:schoolId/:gradeId/:sectionId",subjectController.getSubjectsBySchoolGradeSection);
+router.put("/updateStatus/:id",subjectController.updateStatus);
 
 module.exports = router;
