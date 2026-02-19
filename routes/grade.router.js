d2a7e4db9759082786eb84a460c14a0e8b48c257
Nisha2608 <ashoknisha77@gmail.com>
2026-02-19 16:48:47 +0530

    no changes

--- routes/grade.router.js
+++ routes/grade.router.js
@@ -6,5 +6,6 @@ router.post("/createGrade", gradeController.createGrade);
 router.get("/getAllGrades", gradeController.getAllGrades);
 router.get("/getGradesBySchool/:school_id", gradeController.getGradesBySchool);
 router.delete("/deleteGrade/:id", gradeController.deleteGrade);
+router.put("/updateGrade/:id", gradeController.updateGrade);
 
 module.exports = router;
