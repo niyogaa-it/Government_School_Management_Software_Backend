2e4cd43667e9f142dc0d0492c747c84aaeaed4f9
Nisha2608 <113240426+Nisha2608@users.noreply.github.com>
2026-02-23 16:27:44 +0530

    for fees student fetch udated

--- routes/studenthsc.router.js
+++ routes/studenthsc.router.js
@@ -5,6 +5,7 @@ const studenthsccontroller = require('../controllers/studenthsc.controller');
 router.post('/createStudenthsc', studenthsccontroller.createStudenthsc);
 router.get('/getAllStudenthsc', studenthsccontroller.getAllStudenthsc);
 router.get('/getStudenthscsBySchool/:school_id', studenthsccontroller.getStudenthscsBySchool);
+router.get("/getByAdmission/:admissionNumber",studenthsccontroller.getStudenthscByAdmission);
 router.get("/getStudenthscById/:id",studenthsccontroller.getStudenthscById);
 router.put("/updateStudenthsc/:id", studenthsccontroller.updateStudenthsc);
 router.put("/updateStatus/:id", studenthsccontroller.updateStatus);
